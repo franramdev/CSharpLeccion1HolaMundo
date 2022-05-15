e7dda4bfc1f1f36534d49549e3dd7f7e4c709e44
Francisco Ram Jimenez Velasquez <franciscoramjive@gmail.com>
2022-05-15 18:55:19 -0500

    Add project files.

---
+++ HolaMundo/Persona.cs
@@ -0,0 +1,38 @@
+﻿namespace HolaMundo
+{
+    public class Persona
+    {
+        //Para propiedades usen PascalCase
+        public string Nombre { get; set; }
+        public short Edad { get; set; }
+        public double Dinero { get; set; }
+        public char Inicial { get; set; }
+
+        //nombres de parametros en metodos van en camelCase
+        public Persona(string nombre, short edad, double dinero)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Dinero = dinero;
+            //Parsing
+            Inicial = char.Parse(Nombre.Substring(0,1));
+        }
+
+        public string SalirACaminar()
+        {
+            return $"{Nombre} está saliendo a caminar";
+        }
+
+        public string Comprar(double precio)
+        {
+            Dinero = Dinero - precio;
+            return $"Has gastado {precio} pesos, eso te deja con {Dinero} pesos";
+        }
+
+        public string CreaMensajeCompleto()
+        {
+            return $"Hola Mundo soy {Nombre} y mi edad es de {Edad} años y mi fortuna es de {Dinero} " +
+                    $"pesos y mi inicial es {Inicial}";
+        }
+    }
+}
---
+++ HolaMundo/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HolaMundo;
+
+
+//string es para guardar texto
+
+string Nombre = "Francisco";
+//el string permite hacer substrings que son pedazos del texto
+string primerosTresCaracteres = Nombre.Substring(0, 3);
+
+//short es para numeros chiquitos, int para medianos, y long para grandes
+short edadenshort = 28;
+int edad = 28;
+long dinero = 9000000000000000000;
+
+//char es para guardar un único caracter
+char primeraLetra = 'F';
+
+//double es para guardar numeros con decimales y decimal es para lo mismo, pero permite más exactitud
+double dineroConDecimalesChiquito = 12500.4234324324324234324342;
+decimal dineroConDecimales = 12500.53123132132132132123132131321321321231156165156M;
+
+//El booleano solo nos permite guardar si es falso o verdadero
+bool miBooleano = true;
+
+DateTime ahora = DateTime.Now;
+DateTime hoy = DateTime.Now.Date;
+int year = ahora.Year;
+int month = ahora.Month;
+int day = ahora.Day;
+
+Persona yo = new Persona("Francisco",28,543235.12);
+Persona joe = new Persona("Joe",31,110000000000.5);
+
+
+
+//Esta instruccion nos permite imprimir en la consola
+Console.WriteLine(joe.CreaMensajeCompleto());
+Console.WriteLine(yo.CreaMensajeCompleto());
+
+//Console.WriteLine(yo.SalirACaminar());
+//Console.WriteLine(yo.Comprar(5000));
+
